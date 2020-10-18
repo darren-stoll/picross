968875513cefb26e86789e0c8a927aa0e083fd02
darren-stoll <darrenstoll11@gmail.com>
2020-10-17 22:16:30 -0600

    First commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const MainPage = () => {
+  let picrossCellArray = [];
+  // Row
+  for (let i = 0; i < 16; i++) {
+    // Column
+    let columnArray = [];
+    for (let j = 0; j < 16; j++) {
+      let cellKey = "";
+      if (i < 10) cellKey += "0";
+      cellKey += i.toString();
+      if (j < 10) cellKey += "0";
+      cellKey += j.toString();
+      columnArray.push(<Cell key={cellKey} cellKey={cellKey} row={i} col={j} />);
+    }
+    picrossCellArray.push(columnArray);
+  }
+
+
+  let keyCounter = 0;
+
+  return (
+    <div className="main">
+      <div className="picrossBoard">
+        {picrossCellArray.map(element => {
+          let keyName = `picrossCol${keyCounter}`;
+          keyCounter++;
+          return (
+            <div key={keyName} className="picrossCol">
+              {element.map(e => {
+                return e;
+              })}
+            </div>
+          )
+          
+        })}
+      </div>
+    </div>
+  )
+}
+
+const Cell = ({ cellKey, row, col }) => {
+  const [active, setActive] = React.useState(true);
+
+  let cellClass = active ? "picCell" : "picCellActive";
+  
+  return (
+    <div id={cellKey} className={cellClass} onClick={() => {
+      setActive(!active)
+    }}></div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainPage />
+    </>
   );
 }
 
